2fc37fbde254f3583796322f90b27a929ae950fb
Samiksha Upase <samikshaupase09@gmail.com>
2026-02-17 22:34:11 +0530

    EF Core- Code First Demo

---
+++ DAY12/EFCoreDemo/Migrations/20260217165024_UpdatedEmployeeModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedEmployeeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Product = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ DAY12/EFCoreDemo/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Data.SqlClient; 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+CrmContext _context = new CrmContext();
+
+var employees = _context.Employee
+      .Where(e=>e.Age>25)
+      .ToList();
+
+// Without ToList()
+// Console.WriteLine("customers.GetType(): " + customers.GetType());
+// Console.WriteLine($"Query: {customers.ToQueryString()}");
+
+// With ToList()
+// Console.WriteLine("customers.GetType(): " + customers.GetType());
+
+// With AsEnumerable()
+// Console.WriteLine("customers.GetType(): " + customers.GetType());
+
+// employees.Add(new Employee { Name = "John Doe", Age = 30 });
+// _context.SaveChanges();
+
+Console.WriteLine($" Employee Count: {employees.Count()}");
+
+employees.Add(new Employee { Name = "Danny Lee", Age = 30 });
+_context.SaveChanges();
+
+// Because DbSet is not IList, index operator will not work
+// var first = _context.Customers[0];
+
+var john = _context.Employee.FirstOrDefault(e => e.Name == "Rahul");
+if (john != null) john.Age = 31;
+
+_context.SaveChanges();
+
+foreach(var employee in employees)
+{
+    Console.WriteLine($"Id: {employee.Id} Employee:{employee.Name},Age:{employee.Age}");
+}
+
+class CrmContext :DbContext
+{
+    public DbSet<Employee> Employee {get; set;}
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CrmApplicationDb;Integrated Security=True;TrustServerCertificate=True");
+    }
+}
+public class Order
+{
+    [Key]
+    public int OrderId { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [MinLength(3)]
+    public string Product { get; set; }
+
+    [Required]
+    [Precision(18, 2)]
+    public decimal Price { get; set; }
+
+    [ForeignKey("CustomerId")]
+    public int CustomerId { get; set; }
+    public Employee Employee { get; set; }
+}
+
+public class Employee
+{
+    public int Id{get; set;}
+    public string Name{get; set;}
+    public int Age {get; set;}
+}
